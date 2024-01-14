bc811f392ba57ddf69a72c0de0b9af1579e6848b
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-01-14 12:01:58 -0300

    Criação das rotas para autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Footer } from "./Components/Footer";
-import { Header } from "./Components/Header";
-import { Home } from "./Components/Home";
-import { Login } from "./Components/Login/Login";
+import { Footer } from "./Components/Footer/Footer";
+import { Header } from "./Components/Header/Header";
+import { Home } from "./Pages/Home/Home";
+import { Login } from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
