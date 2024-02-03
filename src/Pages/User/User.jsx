d55ac007fa24ded8e4026a509e8cf813beee7cef
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-02-03 18:45:58 -0300

    criando o user header para nasted navigation do usuário logado

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { UserHeader } from "./components/UserHeader";
+import { Route, Routes } from "react-router-dom";
+import { Feed } from "../Feed/Feed";
+import { UserPhotoPost } from "./UserPhotoPost";
+import { UserStats } from "./UserStats";
 
 export const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
---
+++ src/Pages/User/components/UserHeaderNav.jsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { UserContext } from "../../../UserContext";
+import MinhasFotos from "../../../Assets/feed.svg?react";
+import Estatisticas from "../../../Assets/estatisticas.svg?react";
+import AdicionarFoto from "../../../Assets/adicionar.svg?react";
+import Sair from "../../../Assets/sair.svg?react";
+import styles from "./UserHeaderNav.module.css";
+
+export const UserHeaderNav = () => {
+  const { userLogout } = useContext(UserContext);
+  const [mobile, setMobile] = useState(null);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navigate("/login");
+  }
+
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && "Minhas Fotos"}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && "Estatísticas"}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        {mobile && "Adicionar Foto"}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && "Sair"}
+      </button>
+    </nav>
+  );
+};
