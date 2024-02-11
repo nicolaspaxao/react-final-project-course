d87663c7a39eecae12044568a8c941961a70bd62
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-02-11 15:47:02 -0300

    Adicionado os comments de cada foto

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Enviar from "../../Assets/enviar.svg?react";
+import { useFetch } from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import { ErrorWidget } from "../../Components/Helpers/Error/Error";
+
+export const PhotoCommentsForm = ({ id, setComments }) => {
+  const { error, request } = useFetch();
+  const [comment, setComment] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      ></textarea>
+      <button>
+        <Enviar />
+      </button>
+      <ErrorWidget error={error} />
+    </form>
+  );
+};
